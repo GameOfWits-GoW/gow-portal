fb03e25defeafc61ad404669b0aa76dce2058476
JasonCrk <73272783+JasonCrk@users.noreply.github.com>
2026-01-29 06:59:54 -0500

    feat(mastery-road-experience-panel): sort students alphabetically

--- src/app/portal/classrooms/pages/classroom-admin-panel-overview/panels/experiences/mastery-road-experience-panel/mastery-road-experience-panel.component.ts
+++ src/app/portal/classrooms/pages/classroom-admin-panel-overview/panels/experiences/mastery-road-experience-panel/mastery-road-experience-panel.component.ts
@@ -103,7 +103,10 @@ export class MasteryRoadExperiencePanelComponent implements OnInit, OnDestroy {
   public readonly cancelIcon = X
   public readonly modifyPointsIcon = Bolt
   public readonly applyPenaltyIcon = Gavel
-  public readonly rankingStyles: Record<number, { styleClass: string | null; textIcon: string | null } | undefined> = rankingStyles
+  public readonly rankingStyles: Record<
+    number,
+    { styleClass: string | null; textIcon: string | null } | undefined
+  > = rankingStyles
   public readonly downloadIcon = Download
 
   private destroy$ = new Subject<void>()
@@ -560,7 +563,11 @@ export class MasteryRoadExperiencePanelComponent implements OnInit, OnDestroy {
         next: students => {
           const rankingStudents =
             calcMasteryRoadStudentPeriodStatesRanking(students)
-          this.studentsRanking.set(rankingStudents)
+          this.studentsRanking.set([
+            ...rankingStudents.sort((a, b) =>
+              a.state.firstName.localeCompare(b.state.firstName)
+            )
+          ])
           this.isStudentsLoading.set(false)
         },
         error: err => {
